a62a72e23af0cecc9d5d5d4600ee714987b35f8d
salosek <salosek@nppd.com>
2017-11-27 16:07:06 -0600

    prog assign 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2: Lexical Scoping, saving a matrix inverse in the cache
+## SALoseke
 
-## Write a short comment describing this function
+## This function will save a matrix inverse to cache for future use, when we need it again it
+## can 
 
 makeCacheMatrix <- function(x = matrix()) {
+          minv <- NULL
+          set <- function(y = matrix()) {
+              x <<- y
+              minv <<- NULL
+          }
 
+          get <- function() x
+          setinv <- function(solve) minv <<- solve
+          getinv <- function() minv
+          list(set = set, get = get,
+               setinv = setinv,
+               getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## this function will retreive the inverse if it exists, if not it will compute the inverse and 
+## Save in cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        minv <- x$getinv()
+        if(!is.null(minv)) {
+              message("getting cached inverse")
+              return(minv)
+        }
+        data <- x$get()
+        minv <- solve(data, ...)
+        x$setinv(minv)
+        minv
 }
+
+
+# Examples
+myMat <- matrix(c(4, 2, 7, 6), nrow = 2, ncol = 2)
+myMat2 <- matrix(c(4, 2, 7, 6, 5, 1, 8, 3, 9), nrow = 3, ncol = 3)
+myInv <- makeCacheMatrix(myMat)
+myInv$get()
+myInv$getinv()
+
+myInv$set(myMat2)
+myInv$get()
+
+
+myInv$setinv()
+
+cacheSolve(myInv)
+myInv$getinv()
+myInv$setinv(myMat2)
+myInv$get()
+myInv$getinv()
+
+
+
+
